e41d9d00643e1a92510e0cfe415786fe4a32d8a7
Cory Huynh <cohuynh@UCSD.EDU>
2023-06-01 16:25:15 -0700

    addded assets and screenshot ability

--- Assets/CamScript.cs
+++ Assets/CamScript.cs
@@ -7,6 +7,8 @@ public class CamScript : MonoBehaviour
 
     public GameObject rightController;
     public GameObject dslrCamera;
+    public Camera cam;
+    bool takeShot = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,16 +18,30 @@ public class CamScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger) || Input.GetKeyDown(KeyCode.M))
         {
             dslrCamera.SetActive(true);
             rightController.SetActive(false);
         }
 
-        if (OVRInput.GetUp(OVRInput.Button.SecondaryHandTrigger))
+        if (OVRInput.GetUp(OVRInput.Button.SecondaryHandTrigger) || Input.GetKeyUp(KeyCode.M))
         {
             dslrCamera.SetActive(false);
             rightController.SetActive(true);
+        }
+
+        takeShot = OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) || Input.GetKeyDown(KeyCode.Space);
+
+        if (takeShot)
+        {
+            Texture2D screenShot = new Texture2D(1920, 1080, TextureFormat.RGB24, false);
+            RenderTexture.active = cam.targetTexture;
+            screenShot.ReadPixels(new Rect(0, 0, 1920, 1080), 0, 0);
+            byte[] bytes = screenShot.EncodeToPNG();
+            string filename = "C:/Users/cohuynh/Downloads/picture.png";
+            System.IO.File.WriteAllBytes(filename, bytes);
+            Debug.Log("Saved to: " + filename);
+            takeShot = false;
         }
     }
 }
